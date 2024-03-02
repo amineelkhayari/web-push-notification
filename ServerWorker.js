a39b2e0890a00c0578a85661de39ad96c0ea23f3
AMINE EL KHAYARI <71729731+amineelkhayari@users.noreply.github.com>
2024-03-02 17:09:25 +0000

    update service Worker Data

--- ServerWorker.js
+++ ServerWorker.js
@@ -1,3 +1,15 @@
 self.addEventListener("push", (event) => {
- //
+  //
+  const notification = event.data.json();
+  event.waitUntil(self.registration.showNotification(notification.title, {
+      body: notification.body,
+      icon: "https://github.com/fluidicon.png",
+      data: {
+          notifURL: notification.url
+      }
+  }));
+  });
+self.addEventListener("notificationclick", (event) => {
+  event.waitUntil(clients.openWindow(event.notification.data.notifURL));
+  
 })
